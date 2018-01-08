32cb89559c8ae04443bf8d8566bcb8134d43d2c0
ippei <08173567kanda@gmail.com>
2018-01-08 23:42:36 +0900

    P191~P205 ８章ファイルの読み書き

--- terminal.py
+++ terminal.py
@@ -2,3 +2,29 @@ import os
 my_files = ['accounts.txt', 'detail.csv', 'invite.docx']
 for filenames in my_files:
     print(os.path.join('usr/bin/spam',filenames))
+
+
+import shelve
+shelf_file = shelve.open('mydata')
+cats = ['Zophie', 'Pooka', 'Shimon']
+shelf_file['cats'] = cats
+shelf_file.close
+
+
+
+shelf_file = shelve.open('mydata')
+type(shelf_file)
+shelf_file['cats']
+shelf_file.close
+
+
+import pprint
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc': 'fluffy'}]
+pprint.pformat(cats)
+
+file_obj = open('myCats.py', 'w')
+file_obj.write('cats = ' + pprint.pformat(cats) + '\n')
+file_obj.close
+
+import myCats
+myCats.cats
