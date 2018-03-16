513b6c95a4faf69e27f9aafda311f34e07179d54
ipepi <ipepi1010@gmail.com>
2018-03-16 22:50:09 +0900

    P312 スプレッドシートのデータを読み込む 12.4.3

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -7,7 +7,7 @@ wb = openpyxl.load_workbook('censuspopdata.xlsx')
 sheet = wb.get_sheet_by_name('Population by Census Tract')
 county_data = {}
 
-#TODO :county_dataに郡の人口と地域数を格納する
+#county_dataに郡の人口と地域数を格納する
 print('行を読み込んでいます...')
 for row in range(2, sheet.max_row + 1):
     # スプレットシートの1行に、ひとつの人口調査標準地域のデータがある
@@ -25,5 +25,11 @@ for row in range(2, sheet.max_row + 1):
     #この人口調査標準地域の人口だけ郡の人口を増やす
     county_data[state][county]['pop'] += int(pop)
 
-#TODO: 新しいテキストファイルを開き、country_dataの内容を書き込む
+#新しいテキストファイルを開き、country_dataの内容を書き込む
+print('結果を書き込み中...')
+result_file = open('census2010.py', 'w')
+result_file.write('all_data = ' + pprint.pformat(county_data))
+result_file.close()
+print('完了')
+
 
