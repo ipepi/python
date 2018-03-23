1f2c3c8893df9b4c9bd78f6820a1da1c16f77f54
ipepi <ipepi1010@gmail.com>
2018-03-23 11:48:49 +0900

    13.2.3 多数のPDFから指定したページを結合する-ページを追加する

--- combinePdfs.py
+++ combinePdfs.py
@@ -18,6 +18,9 @@ pdf_writer = PyPDF2.PdfFileWriter()
 for filename in pdf_files:
     pdf_file_obj = open(filename, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-    #TODO: 先頭を除く全てのページをループして追加する
+    #先頭を除く全てのページをループして追加する
+    for page_num in range(1, pdf_reader.numPages):
+        page_obj = pdf_reader.getPage(page_num)
+        pdf_writer.addPage(page_obj)
 
 #TODO: 統合したPDFをファイルに保存する
