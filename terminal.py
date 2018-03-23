e552410813fb87c3e8550a7b670afe08bc8a3872
ipepi <ipepi1010@gmail.com>
2018-03-23 11:43:25 +0900

    P335〜344 PDFの基本操作(ページ追加、回転、統合、暗号化)

--- terminal.py
+++ terminal.py
@@ -197,7 +197,7 @@ wb.save('sampleChart.xlsx')
 
 
 import PyPDF2
-:pdf1_file = open('meetingminutes.pdf', 'rb')
+pdf1_file = open('meetingminutes.pdf', 'rb')
 pdf2_file = open('meetingminutes2.pdf', 'rb')
 pdf1_reader = PyPDF2.PdfFileReader(pdf1_file)
 pdf2_reader = PyPDF2.PdfFileReader(pdf2_file)
@@ -207,7 +207,7 @@ for page_num in range(pdf1_reader.numPages):
     pdf_writer.addPage(page_obj)
 
 
-for page_num n range(pdf2_reader.numPages):
+for page_num in range(pdf2_reader.numPages):
     page_obj = pdf2_reader.getPage(page_num)
     pdf_writer.addPage(page_obj)
 
@@ -219,4 +219,49 @@ pdf1_file.close()
 pdf2_file.close()
 
 
+import PyPDF2
+minutes_file = open('meetingminutes.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(minutes_file)
+page = pdf_reader.getPage(0)
+page.rotateClockwise(90)
 
+pdf_writer = PyPDF2.PdfFileWriter()
+pdf_writer.addPage(page)
+result_pdf_file = open('rotatedPage.pdf', 'wb')
+pdf_writer.write(result_pdf_file)
+result_pdf_file.close()
+minutes_file.close()
+
+
+
+import PyPDF2
+minutes_file = open('meetingminutes.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(minutes_file)
+minutes_first_page = pdf_reader.getPage(0)
+pdf_watermark_reader = PyPDF2.PdfFileReader(open('watermark.pdf', 'rb'))
+minutes_first_page.mergePage(pdf_watermark_reader.getPage(0))
+pdf_writer = PyPDF2.PdfFileWriter()
+pdf_writer.addPage(minutes_first_page)
+for page_num in range(1, pdf_reader.numPages):
+    page_obj = pdf_reader.getPage(page_num)
+    pdf_writer.addPage(page_obj)
+
+result_pdf_file = open('watermarkedCover.pdf', 'wb')
+pdf_writer.write(result_pdf_file)
+minutes_file.close()
+result_pdf_file.close()
+
+
+
+
+import PyPDF2
+minutes_file = open('meetingminutes.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(minutes_file)
+pdf_writer = PyPDF2.PdfFileWriter()
+for page_num in range(pdf_reader.numPages):
+    pdf_writer.addPage(pdf_reader.getPage(page_num))
+
+pdf_writer.encrypt('swordfish')
+result_pdf = open('encryptedminutes.pdf', 'wb')
+pdf_writer.write(result_pdf)
+result_pdf.close()
